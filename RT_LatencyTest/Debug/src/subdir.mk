315d16126c07ab31daf7b252604ba05fe5967532
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2023-10-26 12:59:37 +0200

    Removed OT_Thread included in the Cyclic task. Updated and removed debug printf. Working.

--- RT_LatencyTest/Debug/src/subdir.mk
+++ RT_LatencyTest/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ OBJS += \
 src/%.o: ../src/%.c src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	arm-buildroot-linux-gnueabihf-gcc -I/home/ubuntu/Desktop/buildroot-2023.08.1/output/host/usr/lib/gcc/arm-buildroot-linux-gnueabihf/12.3.0/plugin/include -I/home/ubuntu/Desktop/buildroot-2023.08.1/output/host/usr/lib -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-buildroot-linux-gnueabihf-gcc -I/home/ubuntu/Desktop/buildroot-2023.08.1/output/host/usr/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- RT_LatencyTest/src/latency_test.c
+++ RT_LatencyTest/src/latency_test.c
@@ -3,16 +3,9 @@
  *
  *  Created on: Oct 24, 2023
  *      Author: ubuntu
- */
-/*
- * rt_task.c
  *
- *  Created on: Oct 24, 2023
- *      Author: ubuntu
- */
-/*
- * POSIX Real Time Example
- * using a single pthread as RT thread
+ * POSIX Real Time Cyclic task
+ * using various pthread as RT threads
  */
 
 //Threads, scheduling, memory stuff
@@ -33,10 +26,18 @@
 int sd; // socket descriptor
 struct sockaddr_in server;
 
-void *thread_func(void *data)
-{
-	printf("Real time task is running...\n");
-	// Open the socket
+//CYCIC TASK TO BE DONE
+void* cyclicTask (void* arg){
+	struct timespec nextCycle, t1, t1_ans;
+	char msg [200];
+	int latency;
+	int length, size;
+	char buffer[4096];
+	clock_gettime(CLOCK_MONOTONIC,&nextCycle);
+	//Since no more thereads will be made or terminated pid is only checked once.
+	pid_t tid = getpid();
+
+	//
 
 	sd = socket (AF_INET, SOCK_DGRAM, 0); // el 0 indica que el programa decide el protocolo que quiere utilizar
 
@@ -58,22 +59,10 @@ void *thread_func(void *data)
 	server.sin_port	= htons(35000); //Server port
 	inet_aton("192.168.250.20", &server.sin_addr.s_addr); //Server address
 
-	return NULL;
-}
-
-//CYCIC TASK TO BE DONE
-void* cyclicTask (void* arg){
-	struct timespec nextCycle, t1, t1_ans;
-	char msg [200];
-	int latency;
-	int length, size;
-	char buffer[4096];
-	clock_gettime(CLOCK_MONOTONIC,&nextCycle);
-	//Since no more thereads will be made or terminated pid is only checked once.
-	pid_t tid = getpid();
+	//
 	while(1){
 		//Next cycle start calculation
-		printf("Cyclic task (PID: %d): Time: %ld s.\n", tid, nextCycle.tv_sec);
+		printf("Cyclic task (PID: %d): Time: %ld s. Latency: %d ns\r", tid,nextCycle.tv_sec,latency);
 
         // Calculate the start time of the next cycle
         nextCycle.tv_nsec += CYCLE_PERIOD_MS * 1000000;  // Convert ms to ns
@@ -89,17 +78,18 @@ void* cyclicTask (void* arg){
     	    }
 
     	sendto(sd, (const char *)msg, sizeof(msg), 0, (struct sockaddr *)&server, sizeof(server));
-    	printf("Sent: %d bytes\n", sizeof(msg));
+    	//printf("Sent: %d bytes\n", sizeof(msg));
 //    	t1= clock_gettime(CLOCK_MONOTONIC,&nextCycle);
     	clock_gettime(CLOCK_MONOTONIC,&t1);
     	// Receiving the confirm message
-    	//size = recvfrom(sd, (char *)buffer, sizeof(buffer), 0, (struct sockaddr *)&server, &length);
+    	size = recvfrom(sd, (char *)buffer, sizeof(buffer), 0, (struct sockaddr *)&server, &length);
     	clock_gettime(CLOCK_MONOTONIC,&t1_ans);
     	latency=t1_ans.tv_nsec-t1.tv_nsec;
     	//printf("Received %d bytes from the server.\n", size);
     	//printf("Received %s \n", buffer);
 		//Wait for the next cycle
-    	printf("Latency: %d \n", latency);
+    	//printf("Latency: %d ns. \r", latency);
+    	fflush (stdout);
 		clock_nanosleep(CLOCK_MONOTONIC,TIMER_ABSTIME,&nextCycle,NULL);
 	}//loop end
 
@@ -141,7 +131,7 @@ int main(int argc, char* argv[])
                 printf("pthread setschedpolicy failed\n");
                 goto out;
         }
-        param.sched_priority = 40;
+        param.sched_priority = 80;
         ret = pthread_attr_setschedparam(&attr, &param);
         if (ret) {
                 printf("pthread setschedparam failed\n");
@@ -154,23 +144,13 @@ int main(int argc, char* argv[])
                 goto out;
         }
 
-        /* Create a pthread of the ONE TIME TASK  with specified attributes */
-        ret = pthread_create(&ot_thread, &attr, thread_func, NULL);
-        if (ret) {
-                printf("create one tiem task pthread failed\n");
-                goto out;
-        }
         /* Create a pthread of the CYCLIC TASK  with NULL attributes */
         ret = pthread_create(&cyclic_thread, NULL , cyclicTask, NULL);
         if (ret) {
                 printf("create cyclic task pthread failed\n");
                 goto out;
         }
-        /* Join the ot_thread and wait until it is done */
-        ret = pthread_join(ot_thread, NULL);
-        if (ret){
-                printf("join pthread failed: %m\n");
-        }
+
         /* Join the cyclic_thread and wait until its done*/
         ret = pthread_join(cyclic_thread, NULL);
         if (ret){
@@ -179,5 +159,4 @@ int main(int argc, char* argv[])
 out:
         return ret;
 }
-
 
